fd672f418e11cff47ad735949b5296bed759be9b
Elijah <loremIpsum1771@users.noreply.github.com>
2025-06-08 00:02:04 -0400

    Update permissions.py - adding dynamic query filters step

--- src/acl/permissions.py
+++ src/acl/permissions.py
@@ -28,3 +28,35 @@ def has_access(user: User, record: dict) -> bool:
 
     # If no rules match, access is denied
     return False
+    
+# --- New Function ---
+def get_acl_filter_clause(user: User) -> (str, tuple):
+    """
+    Generates a SQL WHERE clause and parameters based on the user's role and region.
+    This translates the ACL rules into a database query. 
+    """
+    role = user.role
+    
+    # National users have no filters 
+    if role == 'national':
+        return ("1 = 1", ()) # A clause that is always true
+        
+    # Statal users are filtered by state 
+    elif role == 'statal':
+        return ("state = ?", (user.region,))
+        
+    # Municipal users are filtered by city 
+    elif role == 'municipal':
+        return ("city = ?", (user.region,))
+        
+    # Facilitators can see meetings they host 
+    # (We are simplifying here to just 'host_id' for meetings)
+    elif role == 'facilitator':
+        return ("host_id = ?", (user.id,))
+        
+    # Shadowers can see people they invited, but not meetings by default
+    # We will handle the 'invited_by' case when querying those specific tables.
+    # For a general query on meetings, they see nothing.
+    
+    # Default case: deny access by returning a clause that is always false
+    return ("1 = 0", ())
