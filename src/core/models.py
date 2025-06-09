2d903df3f16c8abef12e2aa6ffbb35c652e79f19
Elijah <loremIpsum1771@users.noreply.github.com>
2025-06-08 21:17:10 -0400

    Update models.py - add Peer class

--- src/core/models.py
+++ src/core/models.py
@@ -1,8 +1,17 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import Optional
 
 @dataclass
 class User:
-    """A simple data class to represent a user for ACL checks."""
+    """A simple data class to represent the current user for ACL checks."""
     id: int
     role: str
     region: str # This can be a city for municipal users, or a state for statal users
+
+@dataclass
+class Peer:
+    """Represents a known peer in the network."""
+    email: str
+    public_key: str # Stored in hex format
+    address: str # e.g., "http://127.0.0.1:5000"
+    last_synced: Optional[int] = None # Unix timestamp
